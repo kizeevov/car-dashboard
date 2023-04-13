14ca533f93253151eaef0867c095f4f013c78296
kizeev.oleg <kizeev.oleg@wb.ru>
2023-04-13 12:02:53 +0700

    GitHub actions testing

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -1,10 +1,17 @@
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 mod player;
 mod ui;
 
 use crate::ui::MainWindow;
 use slint::ComponentHandle;
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 fn main() {
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+
     let window = MainWindow::new().unwrap();
     window.run().unwrap();
 }
